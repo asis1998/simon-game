b724c13dc2ae10779f981d6a5aeac25e5ff30ff6
aashish shrestha <aashish.shrestha00@gmail.com>
2024-07-21 21:38:47 +1000

    random color generated once click event is activated. next step to follow the sequence

--- game.js
+++ game.js
@@ -2,7 +2,6 @@
 
 let randomChosenColor=random();
 switchSound(randomChosenColor);
-
 let gamePattern = [];
 let userClickedPattern = [];
 let level = 0;
@@ -12,11 +11,26 @@ console.log(gamePattern);
 
 
 
+random();
 $(".btn").click(function (event) {
   let userChosenColor = event.target.id;
   userClickedPattern.push(userChosenColor);
+  console.log(userClickedPattern);
   
   switchSound(userChosenColor);
+  if (gamePattern.toString() === userChosenColor.toString()) {
+    console.log("success");
+    
+  } else {
+    console.log("fail");
+  }
+  setTimeout(function () {
+    
+    randomChosenColor= random();
+     gamePattern.push(randomChosenColor);
+     switchSound(randomChosenColor);
+  },500)
+    
   
   return userClickedPattern;
   
@@ -32,47 +46,49 @@ function switchSound(key) {
       greenTune.play();
       $("#green").fadeOut(100).fadeIn(100);
       break;
+      
+      case "red":
+        var redTune = new Audio("./sounds/red.mp3");
+        redTune.play();
+        $("#red").fadeOut(100).fadeIn(100);
+        break;
+        
+        case "yellow":
+          var yellowTune = new Audio("./sounds/yellow.mp3");
+          yellowTune.play();
+          $("#yellow").fadeOut(100).fadeIn(100);
+          break;
+          
+          case "blue":
+            var blueTune = new Audio("./sounds/blue.mp3");
+            blueTune.play();
+            $("#blue").fadeOut(100).fadeIn(100);
+            break;
+            
   
-    case "red":
-      var redTune = new Audio("./sounds/red.mp3");
-      redTune.play();
-      $("#red").fadeOut(100).fadeIn(100);
-      break;
-  
-    case "yellow":
-      var yellowTune = new Audio("./sounds/yellow.mp3");
-      yellowTune.play();
-      $("#yellow").fadeOut(100).fadeIn(100);
-      break;
-  
-    case "blue":
-      var blueTune = new Audio("./sounds/blue.mp3");
-      blueTune.play();
-      $("#blue").fadeOut(100).fadeIn(100);
-      break;
 
-  
-
-    default:
-      console.log(key);
-      break;
-  }
-
-}
-
-function random() {
-  
-  let randomNum = Math.floor(Math.random() * 4);
-  console.log(randomNum);
-  let buttonColors = ["red", "blue", "green", "yellow"];
-  let randomChosenColor = buttonColors[randomNum];
-  return randomChosenColor;
-}
-
-
-
-
-
-
-
-
+            default:
+              console.log(key);
+              break;
+            }
+            
+          }
+          
+          function random() {
+            
+            let randomNum = Math.floor(Math.random() * 4);
+            console.log(randomNum);
+            let buttonColors = ["red", "blue", "green", "yellow"];
+            let randomChosenColor = buttonColors[randomNum];
+            // switchSound(randomChosenColor);
+            return randomChosenColor;
+          }
+          
+          
+          
+          
+          
+          
+          
+          
+          
